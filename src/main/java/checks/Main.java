f227aa0eaeff6df5bf7893f85e0ec7ab036f29fc
Ben <banthony@cs.unm.edu>
2016-03-25 11:23:50 -0600

    Added basic structure for loading html and dummy files.

--- src/main/java/checks/Main.java
+++ src/main/java/checks/Main.java
@@ -1,11 +1,30 @@
 package checks;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main
 {
+	public static String file(String filename)
+	{
+		try
+		{
+			return new String(Files.readAllBytes(Paths.get("src", "main", "resources", "templates", filename)));
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
 	public static void main(String[] args)
 	{
-		get("/hello", (req, res) -> "Hello World");
+		get("/", (req, res) -> file("index.html"));
+		get("/checkin", (req, res) -> file("checkin.html"));
+		get("/checkout", (req, res) -> file("checkout.html"));
 	}
 }
